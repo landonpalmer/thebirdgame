07b1b2c43adca6bc68a15ff5583e7a8bc40b713a
Landon Palmer <landonjpalmer@gmail.com>
2024-05-18 11:54:11 -0500

    including onclick with constants

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import * as React from "react";
+import { useState } from "react";
 import WorldMap from "react-svg-worldmap";
 import "./App.css";
-import { CountryCodes } from "./constants/countryCodes";
+import { CountryCodes } from "./constants/CountryCodes";
 
-function App() {
-  const data = CountryCodes;
+
+export default function App() {
+
+  const countryData = CountryCodes;
+
+  const [state, setState] = useState({
+    cName: "Select Country",
+    iso: "",
+    val: "",
+  });
+
+  const clickAction = React.useCallback(
+    ({ countryName, countryCode, countryValue }) => {
+      setState({
+        cName: countryName,
+        iso: countryCode,
+        val: "test",
+      });
+    },
+    [],
+  );
 
   return (
     <div className="App">
@@ -15,17 +35,29 @@ function App() {
           className="image"
         />
         <WorldMap
-          color="red"
           title="The Bird Game"
           value-suffix="people"
           size="xl"
-          richInteraction="true"
-          data={data}
+          onClickFunction={clickAction}
+          data={[
+            { country: "cn", value: 1389618778 }, // china
+            { country: "in", value: 1311559204 }, // india
+            { country: "us", value: 331883986 }, // united states
+            { country: "id", value: 264935824 }, // indonesia
+            { country: "pk", value: 210797836 }, // pakistan
+            { country: "br", value: 210301591 }, // brazil
+            { country: "ng", value: 208679114 }, // nigeria
+            { country: "bd", value: 161062905 }, // bangladesh
+            { country: "ru", value: 141944641 }, // russia
+            { country: "mx", value: 127318112 }, // mexico
+          ]}
         />
+        <ul>
+        <li>Country: {state.cName}</li>
+        <li>ISO Code: {state.iso}</li>
+        <li>GDP per capita: {state.val}</li>
+      </ul>
       </div>
     </div>
   );
 }
-
-export default App;
-
---
+++ src/constants/CountryCodes.js
@@ -0,0 +1,5 @@
+// Use the new type for the CountryCodes array
+  export const CountryCodes = [
+    { value: 1, label: 'China' },
+    { value: 2, label: 'India' },
+  ];
