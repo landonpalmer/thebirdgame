4475c9a22d1576082bcc9299c01a03761e7c767a
Landon Palmer <landonjpalmer@gmail.com>
2024-05-18 11:12:19 -0500

    constants addition with image test

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import * as React from "react";
 import WorldMap from "react-svg-worldmap";
 import "./App.css";
+import { CountryCodes } from "./constants/countryCodes";
 
 function App() {
-  const data = [
-    { country: "cn", value: 1389618778 }, // china
-    { country: "in", value: 1311559204 }, // india
-    { country: "us", value: 331883986 }, // united states
-    { country: "id", value: 264935824 }, // indonesia
-    { country: "pk", value: 210797836 }, // pakistan
-    { country: "br", value: 210301591 }, // brazil
-    { country: "ng", value: 208679114 }, // nigeria
-    { country: "bd", value: 161062905 }, // bangladesh
-    { country: "ru", value: 141944641 }, // russia
-    { country: "mx", value: 127318112 }, // mexico
-  ];
+  const data = CountryCodes;
 
   return (
     <div className="App">
-      <WorldMap
-        color="red"
-        title="Top 10 Populous Countries"
-        value-suffix="people"
-        size="xxl"
-        data={data}
-      /> 
+      <div className="container">
+        <img
+          src="https://www.allaboutbirds.org/guide/assets/photo/303809061-480px.jpg"
+          alt="Bird"
+          className="image"
+        />
+        <WorldMap
+          color="red"
+          title="The Bird Game"
+          value-suffix="people"
+          size="xl"
+          richInteraction="true"
+          data={data}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/constants/countryCodes.js
@@ -0,0 +1,12 @@
+export const CountryCodes = [
+    { country: "cn", value: 1 }, // china
+    { country: "in", value: 1 }, // india
+    { country: "us", value: 100 }, // united states
+    { country: "id", value: 1 }, // indonesia
+    { country: "pk", value: 1 }, // pakistan
+    { country: "br", value: 1 }, // brazil
+    { country: "ng", value: 1 }, // nigeria
+    { country: "bd", value: 1 }, // bangladesh
+    { country: "ru", value: 1 }, // russia
+    { country: "mx", value: 1 }, // mexico
+]
